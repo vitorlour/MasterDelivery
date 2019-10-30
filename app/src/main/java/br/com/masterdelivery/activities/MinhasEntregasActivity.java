b894edc5d315ab0ce993831031adca1481c7dde9
franciscof-lima78 <43150910+franciscof-lima78@users.noreply.github.com>
2019-10-30 17:07:40 -0300

    Update MinhasEntregasActivity.java

--- app/src/main/java/br/com/masterdelivery/activities/MinhasEntregasActivity.java
+++ app/src/main/java/br/com/masterdelivery/activities/MinhasEntregasActivity.java
@@ -2,8 +2,10 @@ package br.com.masterdelivery.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.GridView;
 
 import br.com.masterdelivery.R;
+import br.com.masterdelivery.adapters.RecyclerViewAdapterCorridas;
 
 public class MinhasEntregasActivity extends AppCompatActivity {
 
@@ -11,5 +13,16 @@ public class MinhasEntregasActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_minhas_entregas);
+
+        String lista[] = new String[]{"Conta", "Valor", "Taxa",
+                                      "iFood","100,00", "10,00",
+                                      "iFood", "120,00", "12,00",
+                                      "iFood", "50,00", "5,00",};
+
+        GridView gv = (GridView) findViewById(R.id.grdEntregas);
+
+        gv.setAdapter(new RecyclerViewAdapterCorridas(this,lista));
+
+
     }
 }
